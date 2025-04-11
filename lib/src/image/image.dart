a3952a00ccf690d4cb83875871d14889784a8c6e
jcbbhe <jcbbhe@163.com>
2025-04-11 09:58:48 +0000

    feat:新增image组件

--- lib/src/image/image.dart
+++ lib/src/image/image.dart
@@ -12,10 +12,22 @@ class VantImage extends StatelessWidget {
   final double? height;
 
   /// 占位符
-  final Widget? placeholder;
+  final ImageProvider? placeholder;
 
   /// 加载失败时显示的组件
   final Widget? errorWidget;
+
+  /// 图片适应方式
+  final BoxFit fit;
+
+  /// 圆角半径
+  final double radius;
+
+  /// 是否为圆形
+  final bool round;
+
+  /// 是否启用懒加载
+  final bool lazyLoad;
 
   const VantImage({
     super.key,
@@ -24,20 +36,53 @@ class VantImage extends StatelessWidget {
     this.height,
     this.placeholder,
     this.errorWidget,
+    this.fit = BoxFit.cover,
+    this.radius = 0.0,
+    this.round = false,
+    this.lazyLoad = false,
   });
 
   @override
   Widget build(BuildContext context) {
+    final imageProvider = _getImageProvider();
+
+    return ClipRRect(
+      borderRadius:
+          BorderRadius.circular(round ? (width ?? height ?? 0) / 2 : radius),
+      child: lazyLoad
+          ? _buildLazyLoadImage(imageProvider)
+          : _buildImage(imageProvider),
+    );
+  }
+
+  // 构建普通图片
+  Widget _buildImage(ImageProvider imageProvider) {
     return Image(
-      image: _getImageProvider(),
+      image: imageProvider,
       width: width,
       height: height,
-      fit: BoxFit.cover,
+      fit: fit,
       loadingBuilder: (context, child, loadingProgress) {
         if (loadingProgress == null) return child;
-        return placeholder ?? Center(child: CircularProgressIndicator());
+        return placeholder != null
+            ? Image(image: placeholder!, width: width, height: height, fit: fit)
+            : Center(child: CircularProgressIndicator());
       },
       errorBuilder: (context, error, stackTrace) {
+        return errorWidget ?? Icon(Icons.broken_image, size: width ?? height);
+      },
+    );
+  }
+
+  // 构建懒加载图片
+  Widget _buildLazyLoadImage(ImageProvider imageProvider) {
+    return FadeInImage(
+      placeholder: placeholder ?? AssetImage('assets/placeholder.png'),
+      image: imageProvider,
+      width: width,
+      height: height,
+      fit: fit,
+      imageErrorBuilder: (context, error, stackTrace) {
         return errorWidget ?? Icon(Icons.broken_image, size: width ?? height);
       },
     );
