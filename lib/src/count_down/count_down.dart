a2bb1f817f9cf963557bc65193ca597ad62277d6
jcbbhe <jcbbhe@163.com>
2025-05-09 11:03:58 +0000

    update:调整VanCountDown

--- lib/src/count_down/count_down.dart
+++ lib/src/count_down/count_down.dart
@@ -58,12 +58,14 @@ class VanCountDown extends StatefulWidget {
 
 class _VanCountDownState extends State<VanCountDown> {
   late Duration _remainingTime;
+  late ValueNotifier<String> _formatted;
   Timer? _timer;
 
   @override
   void initState() {
     super.initState();
     _remainingTime = widget.time;
+    _formatted = ValueNotifier(_formatDuration(_remainingTime));
 
     widget.controller?._state = this;
 
@@ -85,9 +87,8 @@ class _VanCountDownState extends State<VanCountDown> {
   @override
   void dispose() {
     _timer?.cancel();
-
+    _formatted.dispose();
     widget.controller?._state = null;
-
     super.dispose();
   }
 
@@ -104,19 +105,19 @@ class _VanCountDownState extends State<VanCountDown> {
         return;
       }
 
-      setState(() {
-        final milliseconds =
-            _remainingTime.inMilliseconds - (widget.millisecond ? 16 : 1000);
+      final milliseconds =
+          _remainingTime.inMilliseconds - (widget.millisecond ? 16 : 1000);
 
-        if (milliseconds <= 0) {
-          _remainingTime = Duration.zero;
-          timer.cancel();
-          widget.onFinish?.call();
-        } else {
-          _remainingTime = Duration(milliseconds: milliseconds);
-          widget.onChange?.call(_remainingTime);
-        }
-      });
+      if (milliseconds <= 0) {
+        _remainingTime = Duration.zero;
+        _formatted.value = _formatDuration(_remainingTime);
+        timer.cancel();
+        widget.onFinish?.call();
+      } else {
+        _remainingTime = Duration(milliseconds: milliseconds);
+        _formatted.value = _formatDuration(_remainingTime);
+        widget.onChange?.call(_remainingTime);
+      }
     });
   }
 
@@ -126,20 +127,15 @@ class _VanCountDownState extends State<VanCountDown> {
 
   void _resetTimer() {
     _timer?.cancel();
-    if (mounted) {
-      setState(() {
-        _remainingTime = widget.time;
-      });
-    }
+    _remainingTime = widget.time;
+    _formatted.value = _formatDuration(_remainingTime);
     if (widget.autoStart) {
       _startTimer();
     }
   }
 
   String _formatDuration(Duration duration) {
-    if (duration.isNegative) {
-      return "00:00:00";
-    }
+    if (duration.isNegative) return "00:00:00";
 
     final format = widget.format;
     int remainingMillis = duration.inMilliseconds;
@@ -149,7 +145,6 @@ class _VanCountDownState extends State<VanCountDown> {
 
     int days = 0, hours = 0, minutes = 0, seconds = 0, millis = 0;
 
-    // 优先计算大单位
     if (format.contains("DD")) {
       days = remainingMillis ~/ Duration.millisecondsPerDay;
       remainingMillis -= days * Duration.millisecondsPerDay;
@@ -166,7 +161,6 @@ class _VanCountDownState extends State<VanCountDown> {
     } else if (!format.contains("HH") &&
         !format.contains("DD") &&
         format.contains("mm")) {
-      // 如果没有小时天数，只写了 mm，则用总分钟数
       minutes = duration.inMinutes;
       remainingMillis = 0;
     }
@@ -178,7 +172,6 @@ class _VanCountDownState extends State<VanCountDown> {
         !format.contains("HH") &&
         !format.contains("DD") &&
         format.contains("ss")) {
-      // 只写了 ss，显示总秒数
       seconds = duration.inSeconds;
       remainingMillis = 0;
     }
@@ -190,26 +183,22 @@ class _VanCountDownState extends State<VanCountDown> {
         .replaceAll('HH', hours.toString())
         .replaceAll('mm', minutes.toString())
         .replaceAll('ss', seconds.toString())
-        .replaceAll('SSS', millis.toString())
-        // 双位数
-        .replaceAll('hh', twoDigits(hours))
-        .replaceAll('ii', twoDigits(minutes))
-        .replaceAll('tt', twoDigits(seconds))
-        // 毫秒补位
-        .replaceAll('ms', threeDigits(millis))
-        .replaceAll('MS', threeDigits(millis).substring(0, 2))
-        .replaceAll('M', threeDigits(millis).substring(0, 1));
+        .replaceAll('SSS', millis.toString());
   }
 
   @override
   Widget build(BuildContext context) {
-    if (widget.child != null) {
-      return widget.child!(_remainingTime);
-    }
-
-    return Text(
-      _formatDuration(_remainingTime),
-      style: Theme.of(context).textTheme.bodyMedium,
+    return ValueListenableBuilder<String>(
+      valueListenable: _formatted,
+      builder: (context, value, _) {
+        if (widget.child != null) {
+          return widget.child!(_remainingTime);
+        }
+        return Text(
+          value,
+          style: Theme.of(context).textTheme.bodyMedium,
+        );
+      },
     );
   }
 }
